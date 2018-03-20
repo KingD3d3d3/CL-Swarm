34a5624333ce7c2ed8da86513341b7e9d9debaf7
KingD3d3d3 <j.bailet@hotmail.fr>
2018-03-20 17:52:02 +0900

    ev

--- homing_task/main_homing.py
+++ homing_task/main_homing.py
@@ -113,13 +113,14 @@ if __name__ == '__main__':
     running = True
     pause = False
     while running:
-        # Check the event queue
-        for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                # The user closed the window or pressed escape
-                running = False
-            if event.type == KEYDOWN and event.key == K_p:
-                pause = not pause  # Pause the game
+        if render:
+            # Check the event queue
+            for event in pygame.event.get():
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                    # The user closed the window or pressed escape
+                    running = False
+                if event.type == KEYDOWN and event.key == K_p:
+                    pause = not pause  # Pause the game
 
         # Pause the game
         if pause:
